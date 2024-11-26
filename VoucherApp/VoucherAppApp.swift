9fc0db3d1d663231a55ed0429c1ad0d003d215b1
Dinith Fernando <dinithofficial01@gmail.com>
2024-11-26 13:00:28 +0530

    Initial Commit

---
+++ VoucherApp/VoucherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoucherAppApp.swift
+//  VoucherApp
+//
+//  Created by Dinith Fernando on 2024-11-26.
+//
+
+import SwiftUI
+
+@main
+struct VoucherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
